cfa583f061180179af00584c1d2920490650a504
whuck <70232597+whuck@users.noreply.github.com>
2020-10-22 14:40:04 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,5 @@
+var num = 0;
+function add() { num++; }
+function show() { alert("You clicked "+num+" times"); }
+function dieRoll() {alert(Math.floor(Math.random() * 6) + 1);}
+function cardDraw() {alert(Math.floor(Math.random() * 52) + 1);}
